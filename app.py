3c03ead1143e8c9c90fd794428dfec823ab9bbd4
karamcu <37547506+karamcu@users.noreply.github.com>
2018-04-15 21:34:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.conf.urls import patterns, include, url
 
 def hello(request):
    text = """<h1>welcome to my app !</h1>"""
    return HttpResponse(text)
+
+
+urlpatterns = patterns('',
+  url(r'^hello/','myapp.app.hello', name='hello'),
+)
