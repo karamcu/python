65f9c0588383099697cc2e9969f8f9551592355f
karamcu <37547506+karamcu@users.noreply.github.com>
2018-04-17 11:04:35 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-  url(r'^hello/','python.app.hello', name='hello'),
+  url(r'^hello/','views.hello', name='hello'),
 )
