191dec8b6cb24e6a313a0bfeb27e919ab6d17964
karamcu <37547506+karamcu@users.noreply.github.com>
2018-04-17 19:35:49 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
+
 #!/usr/bin/env python
+import django
 import os
 import sys
 if __name__ == "__main__":
   os.environ.setdefault("DJANGO_SETTING_MODULE", "myproject.settings")
+  django.setup()
   from django.core.management import execute_from_command_line
   execute_from_command_line(sys.argv)
